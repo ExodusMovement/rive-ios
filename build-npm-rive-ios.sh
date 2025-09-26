9fd7bc4297dcaf88d1fb3b149a1868923c8c17e6
bekatd <bekatd@gmail.com>
2025-09-26 17:25:43 +0400

    chore: migrate to clean semver strategy

--- build-npm-rive-ios.sh
+++ build-npm-rive-ios.sh
@@ -67,13 +67,13 @@ fi
 CURRENT_VERSION=$(node -p "require('$PKG_JSON').version")
 
 if [ "$DO_PUBLISH" = true ]; then
-  if [[ "$CURRENT_VERSION" =~ ^${BASE_VERSION}-exodus\.([0-9]+)$ ]]; then
-    SUFFIX=${BASH_REMATCH[1]}
-    NEXT_SUFFIX=$((SUFFIX + 1))
+  if [ "$CURRENT_VERSION" == "$BASE_VERSION" ]; then
+    IFS='.' read -r major minor patch <<< "$CURRENT_VERSION"
+    patch=$((patch + 1))
+    NEW_VERSION="$major.$minor.$patch"
   else
-    NEXT_SUFFIX=0
+    NEW_VERSION="$BASE_VERSION"
   fi
-  NEW_VERSION="${BASE_VERSION}-exodus.${NEXT_SUFFIX}"
 
   echo "🔢 Current version: $CURRENT_VERSION"
   echo "➡️  Publishing new version: $NEW_VERSION"
